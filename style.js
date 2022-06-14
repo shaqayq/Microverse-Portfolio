6383c889d572f5b592657daaaf36b833312cad2e
shaqayq <shaqayq.darwazi@gmail.com>
2022-06-14 12:10:29 +0430

    Fix eslint errors

--- style.js
+++ style.js
@@ -1,21 +1,21 @@
-function listMenu() {
-   (document.getElementById('dropdown-list').style.display = 'block');
-}
+(function listMenu() {
+  document.getElementById('dropdown-list').style.display = 'block';
+}());
 
-window.onclick = function (event) {
-  var i;
+window.onclick = function hidemenu(event) {
+  let i;
   if (!event.target.matches('.hum_btn')) {
-    let dropdowns = document.getElementsByClassName('dropdown-content');
-    for (i = 0; i < dropdowns.length; i++) {
-      let openDropdown = dropdowns[i];
-      if (openDropdown.style.display = 'block') {
+    const dropdowns = document.getElementsByClassName('dropdown-content');
+    for (i = 0; i < dropdowns.length; i += 1) {
+      const openDropdown = dropdowns[i].style.displayl;
+      if (openDropdown === 'block') {
         openDropdown.style.display = 'none';
       }
     }
   }
 };
 
-function closebtn1() {
+(function closebtn1() {
   const navclose = document.getElementById('dropdown-content');
   navclose.style.display = 'none';
-}
+}());
