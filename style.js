618ab4eac28dd8f4babce6be22fff2f50be69fad
shaqayq <shaqayq.darwazi@gmail.com>
2022-06-14 16:34:26 +0430

    Add menulist

--- style.js
+++ style.js
@@ -1,21 +1,8 @@
-(function listMenu() {
+function listMenu() {
   document.getElementById('dropdown-list').style.display = 'block';
-}());
+}
 
-window.onclick = function hidemenu(event) {
-  let i;
-  if (!event.target.matches('.hum_btn')) {
-    const dropdowns = document.getElementsByClassName('dropdown-content');
-    for (i = 0; i < dropdowns.length; i += 1) {
-      const openDropdown = dropdowns[i].style.displayl;
-      if (openDropdown === 'block') {
-        openDropdown.style.display = 'none';
-      }
-    }
+function closebtn1() {
+    const navclose = document.getElementById('dropdown-content');
+    navclose.style.display = 'none';
   }
-};
-
-(function closebtn1() {
-  const navclose = document.getElementById('dropdown-content');
-  navclose.style.display = 'none';
-}());
