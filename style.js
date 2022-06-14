79cc68db28a673fe8ab6c4a2c39c17db53ce5dd5
shaqayq <shaqayq.darwazi@gmail.com>
2022-06-14 08:10:37 +0430

    fix linter errors

--- style.js
+++ style.js
@@ -1,23 +1,21 @@
-
 function listMenu() {
- var btn= document.getElementById("dropdown-list").style.display="block";
- }
+   (document.getElementById("dropdown-list").style.display = 'block');
+}
 
- window.onclick = function(event) {
-    if (!event.target.matches('.hum_btn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.style.display="block") {
-          openDropdown.style.display="none"
-        }
+window.onclick = function (event) {
+  if (!event.target.matches(".hum_btn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if ((openDropdown.style.display = 'block')) {
+        openDropdown.style.display = 'none';
       }
     }
   }
+};
 
-  function  closebtn1()
-  {
-    let navclose= document.getElementById("dropdown-content")
-     navclose.style.display = "none"
-  }
+function closebtn1() {
+  const navclose = document.getElementById("dropdown-content");
+  navclose.style.display = "none";
+}
