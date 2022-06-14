d678f31afdf95476aee015cf456ca2229a8be187
shaqayq <shaqayq.darwazi@gmail.com>
2022-06-14 14:41:45 +0430

    add files

---
+++ style.js
@@ -0,0 +1,22 @@
+(function listMenu() {
+    document.getElementById('dropdown-list').style.display = 'block';
+  }());
+  
+  window.onclick = function hidemenu(event) {
+    let i;
+    if (!event.target.matches('.hum_btn')) {
+      const dropdowns = document.getElementsByClassName('dropdown-content');
+      for (i = 0; i < dropdowns.length; i += 1) {
+        const openDropdown = dropdowns[i].style.displayl;
+        if (openDropdown === 'block') {
+          openDropdown.style.display = 'none';
+        }
+      }
+    }
+  };
+  
+  (function closebtn1() {
+    const navclose = document.getElementById('dropdown-content');
+    navclose.style.display = 'none';
+  }());
+  
