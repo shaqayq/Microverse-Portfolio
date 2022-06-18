926393d7ffeab48d3f19926727879958b2c0570b
Riyaz Ahmed <r-ahmed@riysaim.com>
2022-06-18 21:00:09 +0530

    fixed close button on popup window

--- style.js
+++ style.js
@@ -12,51 +12,48 @@ function hide() {
 }
 
 const project = {
-    first : {
-      name : 'Tonic',
+  first: {
+    name: 'Tonic',
     jobdescription: {
-      jobname:'Canopy',
-      detail:'Backend dev',
-     'date':2015,
-      counter:'./image/circle.png'
+      jobname: 'Canopy',
+      detail: 'Backend dev',
+      date: 2015,
+      counter: './image/circle.png',
     },
-    image : './image/SnapshootPortfolio.png',
+    image: './image/SnapshootPortfolio.png',
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
     technologies: [
-        'html','css','Javascript'
-        ],
+      'html', 'css', 'Javascript',
+    ],
     liveLink: 'https://shaqayq.github.io/Portfolio/',
-    sourcelink: 'https://github.com/shaqayq/Portfolio.git'
-    },
-    second : {
-      name : 'Multi-Post',
+    sourcelink: 'https://github.com/shaqayq/Portfolio.git',
+  },
+  second: {
+    name: 'Multi-Post',
     jobdescription: {
-      jobname:'Facebook',
-      detail:'Backend dev',
-     'date':2018,
-      counter:'./image/circle.png'
+      jobname: 'Facebook',
+      detail: 'Backend dev',
+      date: 2018,
+      counter: './image/circle.png',
     },
-    image : './image/SnapshootPortfolio2.png',
+    image: './image/SnapshootPortfolio2.png',
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
     technologies: [
-        'html','css','Javascript'
-        ],
+      'html', 'css', 'Javascript',
+    ],
     liveLink: 'https://shaqayq.github.io/Portfolio/',
-    sourcelink: 'https://github.com/shaqayq/Portfolio.git'
-    }
+    sourcelink: 'https://github.com/shaqayq/Portfolio.git',
+  },
 };
 
-function showprojects() { 
- 
-   document.getElementById('test').innerHTML='<h1>hello</h1>';
-  
-  
+function showprojects() {
+  document.getElementById('test').innerHTML = '<h1>hello</h1>';
 }
 
 // function popshow(){
-   
+
 //   const blur = document.getElementById('blure');
-//   blur.classList.add('pop-background'); 
+//   blur.classList.add('pop-background');
 //   document.getElementById('popupwindow').style.display= 'block';
 //   document.getElementById('pop-title').innerHTML= project.name;
 //   document.getElementById('pop-img').src= project.image;
